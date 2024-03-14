a8153c341f452174dc41501102cf18672435547c
Aviral2442 <aviralsrivastava24y15@gmail.com>
2024-03-14 18:37:02 +0530

    frontend

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,4 +1,5 @@
-import { Container, Text, Button, Group } from '@mantine/core';
+import cx from 'clsx';
+import { Title, Text, Container, Button, Overlay } from '@mantine/core';
 // import { GithubIcon } from '@mantinex/dev-icons';
 import classes from './page.module.css';
 import { Icon3dRotate } from '@tabler/icons-react';
@@ -6,42 +7,32 @@ import { Icon3dRotate } from '@tabler/icons-react';
 export default function Home() {
   return (
     <div className={classes.wrapper}>
-      <Container size={'xl'} className={classes.inner}>
-        <h1 className={classes.title}>
-          A{' '}
-          <Text component="span" variant="gradient" gradient={{ from: 'blue', to: 'cyan' }} inherit>
-            fully featured
-          </Text>{' '}
-          React components and hooks library
-        </h1>
+    <Overlay color="#000" opacity={0.65} zIndex={1} />
 
-        <Text className={classes.description} color="dimmed">
-          Build fully functional accessible web applications with ease – Mantine includes more than
-          100 customizable components and hooks to cover you in any situation
+    <div className={classes.inner}>
+      <Title className={classes.title}>
+        Automated AI code reviews for{' '}
+        <Text component="span" inherit className={classes.highlight}>
+          any stack
         </Text>
+      </Title>
 
-        <Group className={classes.controls}>
-          <Button
-            size="xl"
-            className={classes.control}
-            variant="gradient"
-            gradient={{ from: 'blue', to: 'cyan' }}
-          >
-            Get started
-          </Button>
-
-          <Button
-            component="a"
-            href="https://github.com/mantinedev/mantine"
-            size="xl"
-            variant="default"
-            className={classes.control}
-            leftSection={<Icon3dRotate size={20} />}
-          >
-            GitHub
-          </Button>
-        </Group>
+      <Container size={640}>
+        <Text size="lg" className={classes.description}>
+          Build more reliable software with AI companion. AI is also trained to detect lazy
+          developers who do nothing and just complain on Twitter.
+        </Text>
       </Container>
+
+      <div className={classes.controls}>
+        <Button className={classes.control} variant="white" size="lg">
+          Get started
+        </Button>
+        <Button className={cx(classes.control, classes.secondaryControl)} size="lg">
+          Live demo
+        </Button>
+      </div>
     </div>
+  </div>
   );
 }
