c496cdd9e76112d7dbb5243b10832fe8717a3f22
Aviral Srivastava <113462998+Aviral2442@users.noreply.github.com>
2024-03-14 17:48:17 +0530

    Initial commit

---
+++ frontend/src/app/(main)/login/page.jsx
@@ -0,0 +1,13 @@
+import { Button } from '@mantine/core';
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div>Login
+
+      <Button variant="filled">Button</Button>
+    </div>
+  )
+}
+
+export default Login;
---
+++ frontend/src/app/page.jsx
@@ -0,0 +1,47 @@
+import { Container, Text, Button, Group } from '@mantine/core';
+// import { GithubIcon } from '@mantinex/dev-icons';
+import classes from './page.module.css';
+import { Icon3dRotate } from '@tabler/icons-react';
+
+export default function Home() {
+  return (
+    <div className={classes.wrapper}>
+      <Container size={'xl'} className={classes.inner}>
+        <h1 className={classes.title}>
+          A{' '}
+          <Text component="span" variant="gradient" gradient={{ from: 'blue', to: 'cyan' }} inherit>
+            fully featured
+          </Text>{' '}
+          React components and hooks library
+        </h1>
+
+        <Text className={classes.description} color="dimmed">
+          Build fully functional accessible web applications with ease – Mantine includes more than
+          100 customizable components and hooks to cover you in any situation
+        </Text>
+
+        <Group className={classes.controls}>
+          <Button
+            size="xl"
+            className={classes.control}
+            variant="gradient"
+            gradient={{ from: 'blue', to: 'cyan' }}
+          >
+            Get started
+          </Button>
+
+          <Button
+            component="a"
+            href="https://github.com/mantinedev/mantine"
+            size="xl"
+            variant="default"
+            className={classes.control}
+            leftSection={<Icon3dRotate size={20} />}
+          >
+            GitHub
+          </Button>
+        </Group>
+      </Container>
+    </div>
+  );
+}
