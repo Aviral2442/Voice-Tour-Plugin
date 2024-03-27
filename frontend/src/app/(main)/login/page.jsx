e66b1b2a1762a89c67367d94a4bf96b6392cc679
Aviral2442 <aviralsrivastava24y15@gmail.com>
2024-03-27 17:16:17 +0530

    frontend

--- frontend/src/app/(main)/login/page.jsx
+++ frontend/src/app/(main)/login/page.jsx
@@ -43,7 +43,7 @@ export function Login(props) {
     },
 
     validate: {
-      email: val => (/^\S+@\S+$/.test(val) ? null : "Invalid email"),
+      email: val => (/^\S+@\S+$/.test(val) ? null : "Invalid email"), 
       password: val =>
         val.length <= 6 ? "Password should include at least 6 characters" : null
     }
