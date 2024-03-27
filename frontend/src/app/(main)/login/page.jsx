e63bea8011a9368c52330079dd20dfa9843013cb
sachin2745 <sachin.official648@gmail.com>
2024-03-27 17:15:34 +0530

    login updated

--- frontend/src/app/(main)/login/page.jsx
+++ frontend/src/app/(main)/login/page.jsx
@@ -1,7 +1,7 @@
 'use client'
 import React from 'react';
 import { useToggle, upperFirst } from "@mantine/hooks"
-import { useForm } from "@mantine/form"
+import { Form, useForm } from "@mantine/form"
 import {
   TextInput,
   PasswordInput,
@@ -12,13 +12,17 @@ import {
   Divider,
   Checkbox,
   Anchor,
-  Stack
+  Stack,
+  Title
 } from "@mantine/core"
 import { GoogleButton } from "./GoogleButton"
 import { TwitterButton } from "./TwitterButton"
 import classes from './login.module.css';
 import cx from 'clsx';
 import { MantineProvider, Container, createTheme } from '@mantine/core';
+import Link from 'next/link';
+import { BackgroundImage, Center, Box } from '@mantine/core';
+
 
 const theme = createTheme({
   components: {
@@ -31,113 +35,79 @@ const theme = createTheme({
 });
 
 
-export function Login(props) {
-
-  const [type, toggle] = useToggle(["login", "register"])
+export function Login() {
   const form = useForm({
     initialValues: {
-      email: "",
-      name: "",
-      password: "",
-      terms: true
+      email: '',
+      password: '',
+      terms: true,
+
     },
 
     validate: {
-      email: val => (/^\S+@\S+$/.test(val) ? null : "Invalid email"),
-      password: val =>
-        val.length <= 6 ? "Password should include at least 6 characters" : null
-    }
-  })
+      email: (value) => (/^\S+@\S+$/.test(value) ? null : 'Invalid email'),
+      password: (value) => (value.length <= 8 ? 'Password should include at least 8 characters' : null),
 
-  
+    },
+  });
+
+
 
   return (
     <MantineProvider theme={theme} >
-      <Container size="responsive" px={0} bg="white" mt={50}>
-        <Paper radius="md" p="xl" withBorder {...props} h={500} size="xxs" className={classes.Container}>
-          <Text size="lg" fw={500}>
-            Welcome to Mantine, {type} with
-          </Text>
+      <Box mx="auto" >
+        <BackgroundImage
+          src="https://t3.ftcdn.net/jpg/03/55/60/70/360_F_355607062_zYMS8jaz4SfoykpWz5oViRVKL32IabTP.jpg"
+          radius="md"
+          h={700}
+        >
+          <Center p="md">
+            <Container size="responsive" mt={50} h={700} w={700} >
+              <Paper withBorder shadow="md" p={30} mt={30} radius="md" p="xl" withBorder className={classes.Paper}>
+                <Title className={classes.title} >
+                  Welcome to Mantine, Login with
+                </Title>
+                <Group grow mb="md" mt="md">
+                  <GoogleButton radius="xl" variant="outline" color="rgba(0, 0, 0, 1)">Google</GoogleButton>
+                  <TwitterButton radius="xl" variant="outline" color="rgba(0, 0, 0, 1)">Twitter</TwitterButton>
+                </Group>
 
-          <Group grow mb="md" mt="md">
-            <GoogleButton radius="xl">Google</GoogleButton>
-            <TwitterButton radius="xl">Twitter</TwitterButton>
-          </Group>
+                <Divider label="Or continue with email" labelPosition="center" my="lg" />
+                <form onSubmit={form.onSubmit((values) => console.log(values))}>
 
-          <Divider label="Or continue with email" labelPosition="center" my="lg" />
-
-          <form onSubmit={form.onSubmit(() => { })}>
-            <Stack>
-              {type === "register" && (
-                <TextInput
-                  label="Name"
-                  placeholder="Your name"
-                  value={form.values.name}
-                  onChange={event =>
-                    form.setFieldValue("name", event.currentTarget.value)
-                  }
-                  radius="md"
-                />
-              )}
-
-              <TextInput
-                required
-                label="Email"
-                placeholder="hello@mantine.dev"
-                value={form.values.email}
-                onChange={event =>
-                  form.setFieldValue("email", event.currentTarget.value)
-                }
-                error={form.errors.email && "Invalid email"}
-                radius="md"
-              />
-
-              <PasswordInput
-                required
-                label="Password"
-                placeholder="Your password"
-                value={form.values.password}
-                onChange={event =>
-                  form.setFieldValue("password", event.currentTarget.value)
-                }
-                error={
-                  form.errors.password &&
-                  "Password should include at least 6 characters"
-                }
-                radius="md"
-              />
-
-              {type === "register" && (
-                <Checkbox
-                  label="I accept terms and conditions"
-                  checked={form.values.terms}
-                  onChange={event =>
-                    form.setFieldValue("terms", event.currentTarget.checked)
-                  }
-                />
-              )}
-            </Stack>
-
-            <Group justify="space-between" mt="xl">
-              <Anchor
-                component="button"
-                type="button"
-                c="dimmed"
-                onClick={() => toggle()}
-                size="xs"
-              >
-                {type === "register"
-                  ? "Already have an account? Login"
-                  : "Don't have an account? Register"}
-              </Anchor>
-              <Button type="submit" radius="xl" variant="gradient"
-                  gradient={{ from: 'gray', to: 'red', deg: 11 }}>
-                {upperFirst(type)}
-              </Button>
-            </Group>
-          </form>
-        </Paper>
-      </Container>
+                  <TextInput withAsterisk label="Email" placeholder="your@email.com"
+                    {...form.getInputProps('email')} required />
+                  <PasswordInput label="Password"
+                    placeholder="Your password"
+                    value={form.values.password}
+                    onChange={(event) => form.setFieldValue('password', event.currentTarget.value)}
+                    error={form.errors.password && 'Password should include at least 8 characters'}
+                    required mt="md" />
+                    
+                  <Group justify="space-between" mt="lg">
+                    <Checkbox label="Remember me"
+                       checked={form.values.terms}
+                       onChange={(event) => form.setFieldValue('terms', event.currentTarget.checked)}
+                    />
+                    <Anchor component="button" size="sm">
+                      Forgot password?
+                    </Anchor>
+                  </Group>
+                  <Group justify="space-between" mt="xl">
+                    <Anchor component={Link} underline="hover" type="button" c="dimmed" href="/signup" size="xs">
+                      Don't have an account? Register
+                    </Anchor>
+                    <Button type="submit" variant="outline" color="rgba(0, 0, 0, 1)"
+                    >
+                      Login
+                    </Button>
+                  </Group>
+                </form>
+              </Paper>
+            </Container>
+          </Center>
+        </BackgroundImage>
+      </Box>
     </MantineProvider>
   )
 }
