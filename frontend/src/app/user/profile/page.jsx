3afdc4f5a7f3b2231d23f4df642aac3f793b44e6
Aviral2442 <aviralsrivastava24y15@gmail.com>
2024-04-03 07:59:52 +0530

    webpage form create

--- frontend/src/app/user/profile/page.jsx
+++ frontend/src/app/user/profile/page.jsx
@@ -1,9 +1,52 @@
+'use client'
 import React from 'react'
+import {
+  TextInput,
+  PasswordInput,
+  Checkbox,
+  Anchor,
+  Paper,
+  Title,
+  Text,
+  Container,
+  Group,
+  Button
+} from "@mantine/core"
+import classes from './webPage.module.css'
 
-const UserProfile = () => {
+const WebPage = () => {
   return (
-    <div>UserProfile</div>
+    <Container size={420} my={40}>
+    <Title ta="center" className={classes.title}>
+     Web Page For user
+    </Title>
+    <Text c="dimmed" size="sm" ta="center" mt={5}>
+      Do not have an account yet?{" "}
+      <Anchor size="sm" component="button">
+        Fill the Form
+      </Anchor>
+    </Text>
+
+    <Paper withBorder shadow="md" p={30} mt={30} radius="md">
+      <TextInput label="Email" placeholder="you@mantine.dev" required />
+      <PasswordInput
+        label="Password"
+        placeholder="Your password"
+        required
+        mt="md"
+      />
+      <Group justify="space-between" mt="lg">
+        <Checkbox label="Remember me" />
+        <Anchor component="button" size="sm">
+          Forgot password?
+        </Anchor>
+      </Group>
+      <Button fullWidth mt="xl">
+        Sign in
+      </Button>
+    </Paper>
+  </Container>
   )
 }
 
-export default UserProfile;
+export default WebPage;
