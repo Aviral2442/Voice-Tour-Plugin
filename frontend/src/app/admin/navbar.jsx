823c883646f84caca05bf908e3cd2f2e236fe6ee
sachin2745 <sachin.official648@gmail.com>
2024-04-04 18:08:59 +0530

    sidebar and layout

--- frontend/src/app/admin/navbar.jsx
+++ frontend/src/app/admin/navbar.jsx
@@ -1,8 +1,252 @@
+'use client'
 import React from 'react'
+import {
+  HoverCard,
+  Group,
+  Button,
+  UnstyledButton,
+  Text,
+  SimpleGrid,
+  ThemeIcon,
+  Anchor,
+  Divider,
+  Center,
+  Box,
+  Burger,
+  Drawer,
+  Collapse,
+  ScrollArea,
+  rem,
+  useMantineTheme,
+  Title,
+  TextInput,
+  Code,
+} from '@mantine/core';
+import { MantineLogo } from '@mantinex/mantine-logo';
+import { useDisclosure } from '@mantine/hooks';
+import {
+  IconNotification,
+  IconCode,
+  IconBook,
+  IconChartPie3,
+  IconFingerprint,
+  IconCoin,
+  IconChevronDown,
+  IconSearch,
+} from '@tabler/icons-react';
+import classes from './adminNavbar.module.css';
+import Link from 'next/link';
+import cx from 'clsx';
+import { ActionIcon, useMantineColorScheme, useComputedColorScheme } from '@mantine/core';
+import { IconSun, IconMoon } from '@tabler/icons-react';
 
-const AdminNavbar = () => {
+const mockdata = [
+  {
+    icon: IconCode,
+    title: 'Open source',
+    description: 'This Pokémon’s cry is very loud and distracting',
+  },
+  {
+    icon: IconCoin,
+    title: 'Free for everyone',
+    description: 'The fluid of Smeargle’s tail secretions changes',
+  },
+  {
+    icon: IconBook,
+    title: 'Documentation',
+    description: 'Yanma is capable of seeing 360 degrees without',
+  },
+  {
+    icon: IconFingerprint,
+    title: 'Security',
+    description: 'The shell’s rounded shape and the grooves on its.',
+  },
+  {
+    icon: IconChartPie3,
+    title: 'Analytics',
+    description: 'This Pokémon uses its flying ability to quickly chase',
+  },
+  {
+    icon: IconNotification,
+    title: 'Notifications',
+    description: 'Combusken battles with the intensely hot flames it spews',
+  },
+];
+
+function AdminNavbar() {
+  const [drawerOpened, { toggle: toggleDrawer, close: closeDrawer }] = useDisclosure(false);
+  const [linksOpened, { toggle: toggleLinks }] = useDisclosure(false);
+  const theme = useMantineTheme();
+
+  const { setColorScheme } = useMantineColorScheme()
+  const computedColorScheme = useComputedColorScheme('light', { getInitialValueInEffect: true });
+
+  const links = mockdata.map((item) => (
+    <UnstyledButton className={classes.subLink} key={item.title}>
+      <Group wrap="nowrap" align="flex-start">
+        <ThemeIcon size={34} variant="default" radius="md">
+          <item.icon style={{ width: rem(22), height: rem(22) }} color={theme.colors.blue[6]} />
+        </ThemeIcon>
+        <div>
+          <Text size="sm" fw={500}>
+            {item.title}
+          </Text>
+          <Text size="xs" c="dimmed">
+            {item.description}
+          </Text>
+        </div>
+      </Group>
+    </UnstyledButton>
+
+
+  ));
+
   return (
-    <div>AdminNavbar</div>
+    <Box pb={0}>
+      <header className={classes.header}>
+        <Group justify='space-between' h="100%">
+          <Anchor href="http://localhost:3000/" underline="never">
+            <Title order={3} >VoiceTour Navigator </Title>
+          </Anchor>
+
+
+          <Group visibleFrom="sm">
+            <Group h="100%" gap={0} visibleFrom="sm">
+              <a href="http://localhost:3000/" className={classes.link}>
+                Home
+              </a>
+              <a href="/voiceAssistant" className={classes.link}>
+                Voice Assistant
+              </a>
+              <a href="/tourGenerator" className={classes.link}>
+                Tour Generator
+              </a>
+              <a href="#" className={classes.link}>
+                Documentation
+              </a>
+              {/* <HoverCard width={600} position="bottom" radius="md" shadow="md" withinPortal>
+                <HoverCard.Target>
+                  <a href="#" className={classes.link}>
+                    <Center inline>
+                      <Box component="span" mr={5}>
+                        Features
+                      </Box>
+                      <IconChevronDown
+                        style={{ width: rem(16), height: rem(16) }}
+                        color={theme.colors.blue[6]}
+                      />
+                    </Center>
+                  </a>
+                </HoverCard.Target>
+
+                <HoverCard.Dropdown style={{ overflow: 'hidden' }}>
+                  <Group justify="space-between" px="md">
+                    <Text fw={500}>Features</Text>
+                    <Anchor href="#" fz="xs">
+                      View all
+                    </Anchor>
+                  </Group>
+
+                  <Divider my="sm" />
+
+                  <SimpleGrid cols={2} spacing={0}>
+                    {links}
+                  </SimpleGrid>
+
+                  <div className={classes.dropdownFooter}>
+                    <Group justify="space-between">
+                      <div>
+                        <Text fw={500} fz="sm">
+                          Get started
+                        </Text>
+                        <Text size="xs" c="dimmed">
+                          Their food sources have decreased, and their numbers
+                        </Text>
+                      </div>
+                      <Button variant="default">Get started</Button>
+                    </Group>
+                  </div>
+                </HoverCard.Dropdown>
+              </HoverCard> */}
+              <a href="/about" className={classes.link}>
+                About us
+              </a>
+              <a href="/contact" className={classes.link}>
+                Contact us
+              </a>
+            </Group>
+            <TextInput
+              placeholder="Search"
+              size="xs"
+              leftSection={<IconSearch style={{ width: rem(12), height: rem(12) }} stroke={1.5} />}
+              rightSectionWidth={80}
+              // rightSection={<Code className={classes.searchCode}>Ctrl + K</Code>}
+              styles={{ section: { pointerEvents: 'none' } }}
+              mb="sm"
+              mt='md'
+            />
+            {/* <Button component={Link} variant="default" href='/login'>Log in</Button> */}
+            <Button component={Link} href='/signup'>Sign up</Button>
+            <ActionIcon
+              onClick={() => setColorScheme(computedColorScheme === 'light' ? 'dark' : 'light')}
+              variant="default"
+              size="xl"
+              aria-label="Toggle color scheme"
+            >
+              <IconSun className={cx(classes.icon, classes.light)} stroke={1.5} />
+              <IconMoon className={cx(classes.icon, classes.dark)} stroke={1.5} />
+            </ActionIcon>
+          </Group>
+
+          <Burger opened={drawerOpened} onClick={toggleDrawer} hiddenFrom="sm" />
+
+
+        </Group>
+      </header>
+
+      <Drawer
+        opened={drawerOpened}
+        onClose={closeDrawer}
+        size="100%"
+        padding="md"
+        title="Navigation"
+        hiddenFrom="sm"
+        zIndex={1000000}
+      >
+        <ScrollArea h={`calc(100vh - ${rem(80)})`} mx="-md">
+          <Divider my="sm" />
+
+          <a href="#" className={classes.link}>
+            Home
+          </a>
+          <UnstyledButton className={classes.link} onClick={toggleLinks}>
+            <Center inline>
+              <Box component="span" mr={5}>
+                Features
+              </Box>
+              <IconChevronDown
+                style={{ width: rem(16), height: rem(16) }}
+                color={theme.colors.blue[6]}
+              />
+            </Center>
+          </UnstyledButton>
+          <Collapse in={linksOpened}>{links}</Collapse>
+          <a href="#" className={classes.link}>
+            Learn
+          </a>
+          <a href="#" className={classes.link}>
+            Academy
+          </a>
+
+          <Divider my="sm" />
+
+          <Group justify="center" grow pb="xl" px="md">
+            <Button variant="default">Log in</Button>
+            <Button>Sign up</Button>
+          </Group>
+        </ScrollArea>
+      </Drawer>
+    </Box>
   )
 }
 
