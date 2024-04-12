75662b27754070a13fbb02ef3df8bdb7c79099d8
Aviral2442 <aviralsrivastava24y15@gmail.com>
2024-04-13 01:10:58 +0530

    about

--- frontend/src/app/(main)/about/page.jsx
+++ frontend/src/app/(main)/about/page.jsx
@@ -1,8 +1,18 @@
+'use client'
+import { Container, Title } from '@mantine/core';
 import React from 'react'
+import classes from './about.module.css'
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+    <Container  pt={80} >
+      <Title order={1} className={classes.Title} >Voice & Tour Plugin System</Title>
+    </Container>
+    <Container>
+      <p>Welcome to [Your Website Name], where innovation meets convenience in website navigation. Our cutting-edge platform harnesses the power of voice technology to revolutionize the way users interact with websites. With our intuitive voice-based website assistant, users can effortlessly navigate through website content, access information, and perform actions using simple voice commands. Additionally, our interactive website tour generator allows users to explore websites in a guided and personalized manner, enhancing their browsing experience. Our plugin-based system further extends the functionality of our platform, enabling users to customize their experience according to their preferences. Join us on a journey towards seamless website navigation and unparalleled user convenience with [Your Website Name]</p>
+    </Container>
+    </>
   )
 }
 
