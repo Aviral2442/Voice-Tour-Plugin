76552cc31dc21283b6629d7b9db12d61198b3689
sachin2745 <sachin.official648@gmail.com>
2024-04-14 14:58:51 +0530

    HeroBullet Right updated

--- frontend/src/app/HeroBulletRight.jsx
+++ frontend/src/app/HeroBulletRight.jsx
@@ -9,62 +9,61 @@ export function HeroBulletRight() {
     <Container fluid className={classes.Container}>
     <div className={classes.inner}>
         <Group className={classes.Right}>
-            <Text c='green' p={5}>Use Cases</Text><br />
+            <Text c='#66FF00'>Use Cases</Text><br />
             {' '}
             <Title className={classes.Title}> Enhance Your Projects with<br /> Voice Assistant</Title>
             <p className={classes.P}> Discover seamless website navigation like never before with our Voice Assistant feature. Say goodbye to traditional browsing methods and embrace the future of hands-free interaction. Experience convenience, efficiency, and accessibility at your fingertips.</p>
             <Paper shadow="xs" radius="xs" className={classes.paper}>
                 <Flex
                     mih={50}
-                    bg="#1B1919"
+                   
                     gap="md"
                     justify="flex-start"
                     align="flex-start"
                     direction="row"
                     wrap="wrap"
                 >
-                    <Button variant="outline" color="green" radius="md">AI Voice Over for Videos</Button>
-                    <Button variant="outline" color="green" radius="md">Audio Publishing</Button>
-                    <Button variant="outline" color="green" radius="md">Audio story Telling</Button>
+                    <Button variant="outline" color="#66FF00" radius="md">AI Voice Over for Videos</Button>
+                    <Button variant="outline" color="#66FF00" radius="md">Audio Publishing</Button>
+                    <Button variant="outline" color="#66FF00" radius="md">Audio story Telling</Button>
                 </Flex>
                 <Flex
                     mih={50}
-                    bg="#1B1919"
+                   
                     gap="md"
                     justify="flex-start"
                     align="flex-start"
                     direction="row"
                     wrap="wrap"
                 >
-                    <Button variant="outline" color="green" radius="md">Conversational AI</Button>
-                    <Button variant="outline" color="green" radius="md">custom Voice Creation</Button>
-                    <Button variant="outline" color="green" radius="md">E-Learning</Button>
-                    <Button variant="outline" color="green" radius="md">Podcasts</Button>
+                    <Button variant="outline" color="#66FF00" radius="md">Conversational AI</Button>
+                    <Button variant="outline" color="#66FF00" radius="md">custom Voice Creation</Button>
+                    <Button variant="outline" color="#66FF00" radius="md">E-Learning</Button>
+                    <Button variant="outline" color="#66FF00" radius="md">Podcasts</Button>
                 </Flex>
                 <Flex
                     mih={50}
-                    bg="#1B1919"
+                  
                     gap="md"
                     justify="flex-start"
                     align="flex-start"
                     direction="row"
                     wrap="wrap"
                 >
-                    <Button variant="outline" color="green" radius="md">Gaming</Button>
-                    <Button variant="outline" color="green" radius="md">IVR Systems</Button>
-                    <Button variant="outline" color="green" radius="md">Translation & Dubbing</Button>
-                    <Button variant="outline" color="green" radius="md">Voice Accesbility</Button>
+                    <Button variant="outline" color="#66FF00" radius="md">Gaming</Button>
+                    <Button variant="outline" color="#66FF00" radius="md">IVR Systems</Button>
+                    <Button variant="outline" color="#66FF00" radius="md">Translation & Dubbing</Button>
+                    <Button variant="outline" color="#66FF00" radius="md">Voice Accesbility</Button>
                 </Flex>
                 <Flex
                     mih={50}
-                    bg="#1B1919"
                     gap="md"
                     justify="flex-start"
                     align="flex-start"
                     direction="row"
                     wrap="wrap"
                 >
-                    <Button variant="outline" color="green" radius="md">Voice API</Button>
+                    <Button variant="outline" color="#66FF00" radius="md">Voice API</Button>
 
                 </Flex>
             </Paper>
@@ -72,9 +71,9 @@ export function HeroBulletRight() {
 
         <Group >
             <Paper shadow="xs" radius="xs" p="xl" className={classes.Paper}>
-                <Card shadow="sm" padding="lg" radius="md" withBorder c='#1B1919' className={classes.Card}>
+                <Card shadow="sm" padding="lg" radius="md" withBorder bg="white" className={classes.Card}>
                     <Card.Section>
-                        <Image src='micimg.png' className={classes.image} />
+                        <Image src='GAMINg.png' className={classes.image} />
                     </Card.Section>
                 </Card>
             </Paper>
