973850ffa084255a8af7e9eec79a67ec49017f1b
Aviral2442 <aviralsrivastava24y15@gmail.com>
2024-04-15 13:17:29 +0530

    about us

--- frontend/src/app/(main)/about/voiceIntro.jsx
+++ frontend/src/app/(main)/about/voiceIntro.jsx
@@ -1,5 +1,5 @@
 'use client'
-import { Card, Container, Group, Image, Paper, Text, Title } from '@mantine/core'
+import { Card, CardSection, Container, Divider, Group, Image, Paper, Text, Title } from '@mantine/core'
 import React from 'react'
 import classes from './voiceIntro.module.css'
 
@@ -7,6 +7,7 @@ const VoiceIntro = () => {
     return (
 
         <div >
+             
             <Title className={classes.Title} order={2}>Know more About Voice Assistant </Title>
 
             <Container size={1400} className={classes.container}>
@@ -36,6 +37,70 @@ const VoiceIntro = () => {
                 </Group>
 
             </Container>
+
+            <Container size={1400} className={classes.container}>
+
+                <Group className={classes.Left}>
+                    <Paper shadow="xs" radius="xs" p="xl" className={classes.Paper}>
+                        <Card shadow="sm" padding="lg" radius="md" withBorder c='#1B1919' className={classes.Card}>
+                            <Card.Section>
+                                <Image src='voice.png' className={classes.image} />
+                            </Card.Section>
+                        </Card>
+                    </Paper>
+                </Group>
+
+                <Group className={classes.Right}>
+                    {/* <div className={classes.innerSection}> */}
+                        <Container size={1200} p={50}>
+                        <Text  size="lg" >
+                        Welcome to the future of guided digital exploration! Our Tour Generator is at the forefront of revolutionizing user experiences on websites. Designed to enhance understanding and streamline navigation, our technology provides automated, intuitive tours that effortlessly guide users through a website’s content, helping them discover and utilize its full range of features efficiently and enjoyably.
+                        </Text>
+                        <br />
+                        <Text  size="lg" >
+                        Discover the power of exploration with our interactive Tour Generator tool. At Voice & Tour PS, we understand that navigating complex websites can be daunting. That's why we've developed the Tour Generator – a user-friendly solution for creating personalized website tours. Simply specify your areas of interest, set your preferences, and let our Tour Generator do the rest. Whether you're a first-time visitor or a seasoned explorer, our tours offer a curated journey through the most relevant and engaging content on any website. Start exploring with confidence and ease using our Tour Generator.
+                        </Text>
+                        </Container>
+                    {/* </div> */}
+                </Group>
+
+
+            </Container>
+
+
+            <Title className={classes.Title} order={4}>Why Choose </Title>
+
+            <div className={classes.Card}>
+                <Container className={classes.whychoose} >
+                    <Group>
+                        <Card shadow="sm" padding="lg" radius="md" withBorder >
+                            <CardSection p={12} >
+                                <Title c='#66FF00' >Innovative Technology</Title><br />
+                                <Divider size="sm" /><br />
+                                <Text  >Our Voice Assistant uses the latest in AI and speech recognition technology to provide an accurate and efficient browsing experience.</Text>
+                            </CardSection>
+                        </Card>
+                    </Group>
+                    <Group>
+                    <Card shadow="sm" padding="lg" radius="md" withBorder >
+                            <CardSection p={12} >
+                                <Title c='#66FF00' >User-Centric Design</Title><br />
+                                <Divider size="sm" /><br />
+                                <Text  >We design with the user in mind, ensuring our technology is easy to use for everyone, including those with disabilities.</Text>
+                            </CardSection>
+                        </Card>
+                    </Group>
+                    <Group>
+                    <Card shadow="sm" padding="lg" radius="md" withBorder >
+                            <CardSection p={12} >
+                                <Title c='#66FF00' >Continuous Improvement</Title><br />
+                                <Divider size="sm" /><br />
+                                <Text  >We are dedicated to evolving & enhancing our Voice Assistant to meet the growing & changing needs of our users.</Text>
+                            </CardSection>
+                        </Card>
+                    </Group>
+                </Container>
+            </div>
 
         </div>
     )
