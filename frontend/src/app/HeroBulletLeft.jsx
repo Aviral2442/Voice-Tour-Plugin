4a9adfbae650a86c8236fe787594c800211459c4
sachin2745 <sachin.official648@gmail.com>
2024-04-16 19:53:34 +0530

    HeroBullet is responsive

--- frontend/src/app/HeroBulletLeft.jsx
+++ frontend/src/app/HeroBulletLeft.jsx
@@ -20,39 +20,44 @@ const HeroBulletLeft = () => {
 
                 <Group className={classes.Left}>
                     <Text c='#66FF00'  >Use Cases</Text>
-                    <Title className={classes.Title}> Enhance Your Projects with<br /> Voice Assistant</Title>
+                    <Title className={classes.Title}> Enhance Your Projects with Voice Assistant</Title>
                     <p className={classes.P}> Discover seamless website navigation like never before with our Voice Assistant feature. Say goodbye to traditional browsing methods and embrace the future of hands-free interaction. Experience convenience, efficiency, and accessibility at your fingertips.</p>
                     <Paper shadow="xs" radius="xs" className={classes.paper}>
                         <Flex
                             mih={50}
                             gap="md"
+                            wrap={'wrap'}
                             justify="flex-start"
                             align="flex-start"
                             direction="row"
+                            mb={10}
                         >
-                            <Button variant="outline" color="#66FF00" radius="md" >AI Voice Over for Videos</Button>
-                            <Button variant="outline" color="#66FF00" radius="md">Audio Publishing</Button>
-                            <Button variant="outline" color="#66FF00" radius="md">Audio story Telling</Button>
+                            <Button  variant="outline" color="#66FF00" radius="md">AI Voice Over for Videos</Button>
+                            <Button  variant="outline" color="#66FF00" radius="md">Audio Publishing</Button>
+                            <Button  variant="outline" color="#66FF00" radius="md">Audio story Telling</Button>
                         </Flex>
                         <Flex
                             mih={50}
                             gap="md"
+                            wrap={'wrap'}
                             justify="flex-start"
                             align="flex-start"
                             direction="row"
+                            mb={10}
                         >
-                            <Button variant="outline" color="#66FF00" radius="md">Conversational AI</Button>
-                            <Button variant="outline" color="#66FF00" radius="md">custom Voice Creation</Button>
-                            <Button variant="outline" color="#66FF00" radius="md">E-Learning</Button>
-                            <Button variant="outline" color="#66FF00" radius="md">Podcasts</Button>
+                            <Button  variant="outline" color="#66FF00" radius="md">Conversational AI</Button>
+                            <Button  variant="outline" color="#66FF00" radius="md">custom Voice Creation</Button>
+                            <Button  variant="outline" color="#66FF00" radius="md">E-Learning</Button>
+                            <Button  variant="outline" color="#66FF00" radius="md">Podcasts</Button>
                         </Flex>
                         <Flex
                             mih={50}
                             gap="md"
+                            wrap={'wrap'}
                             justify="flex-start"
                             align="flex-start"
                             direction="row"
-                        >
+                            mb={10}>
                             <Button variant="outline" color="#66FF00" radius="md">Gaming</Button>
                             <Button variant="outline" color="#66FF00" radius="md">IVR Systems</Button>
                             <Button variant="outline" color="#66FF00" radius="md">Translation & Dubbing</Button>
@@ -61,10 +66,11 @@ const HeroBulletLeft = () => {
                         <Flex
                             mih={50}
                             gap="md"
+                            wrap={'wrap'}
                             justify="flex-start"
                             align="flex-start"
-                            direction="row"
-                        >
+                            direction="row" 
+                            mb={10}>
                             <Button variant="outline" color="#66FF00" radius="md">Voice API</Button>
 
                         </Flex>
