a6660cf3ece7055c5b9b7742f37949b8e4efda9a
sachin2745 <sachin.official648@gmail.com>
2024-04-18 14:16:57 +0530

    How it works Created in voice assistant

--- frontend/src/app/(main)/voiceAssistant/voiceitWorks.jsx
+++ frontend/src/app/(main)/voiceAssistant/voiceitWorks.jsx
@@ -1,76 +1,94 @@
 import React from 'react'
-import { ThemeIcon, Text, Title, Container, SimpleGrid, rem, Group, Card } from '@mantine/core';
+import { ThemeIcon, Text, Title, Container, SimpleGrid, rem, Group, Card, Center } from '@mantine/core';
 import classes from './voiceitWorks.module.css';
+import { Josefin_Sans, Lora, Playfair_Display } from 'next/font/google';
+import clsx from 'clsx';
+
+
+const font = Josefin_Sans({ subsets: ['latin'], weight: ['400'] });
+
 
 export const MOCKDATA = [
     {
-      title: 'Activate the Voice Assistant',
-      description:
-        'Click the Activate Assistant button or use the voice command to start interacting with our intelligent assistant.This dust is actually a powerful poison that will even make a pro wrestler sick, Regice cloaks itself with frigid air of -328 degrees Fahrenheit ' ,
+        title: 'Activate the Voice Assistant',
+        description:
+            'Click the Activate Assistant button or use the voice command to start interacting with our intelligent assistant.',
+            Number:'1',
     },
     {
-      title: 'Privacy focused',
-      description:
-        'People say it can run at the same speed as lightning striking, Its icy body is so cold, it will not melt even if it is immersed in magma',
+        title: 'Navigate Websites',
+        description:
+            ' Speak your desired commands to navigate through website pages, access links, or perform actions such as searching or filling out forms.',
+            Number:'2',
     },
     {
-      title: 'No third parties',
-      description:
-        'They’re popular, but they’re rare. Trainers who show them off recklessly may be targeted by thieves',
+        title: 'Explore Features',
+        description:
+            'Discover additional features of the Voice Assistant, such as voice-controlled scrolling, zooming, and page navigation.',
+            Number:'3',
     },
-    {
-     
-      title: 'Secure by default',
-      description:
-        'Although it still can’t fly, its jumping power is outstanding, in Alola the mushrooms on Paras don’t grow up quite right',
-    },
-    {
-      
-      title: '24/7 Support',
-      description:
-        'Rapidash usually can be seen casually cantering in the fields and plains, Skitty is known to chase around after its own tail',
-    },
-  ];
-  
- 
-  export function Feature({  title, description }) {
-    return (
-      <div>
-       
-        <Text mt="sm" mb={7}>
-          {title}
-        </Text>
-        <Text size="sm" c="dimmed" lh={1.6}>
-          {description}
-        </Text>
-      </div>
-    );
-  }  
 
-  export function VoiceitWorks() {
+    {
+
+        title: 'Customize Settings',
+        description:
+            ' Personalize your Voice Assistant experience by adjusting settings such as voice sensitivity, language preferences, and command recognition.',
+            Number:'4',
+
+    },
+    {
+
+        title: 'Compatibility',
+        description:
+            ' Our Voice Assistant is compatible with most modern web browsers and websites. While we strive to ensure compatibility across a wide range of platforms, please note that certain website structures or functionalities may impact the performance of the Voice Assistant.',
+            Number:'5',
+    },
+];
+
+
+export function Feature({ title, description,Number }) {
+    return (
+        <div>
+            <Card h={200} className={classes.Card}>
+                <Text mt="sm" fz={24} mb={7} c={"#66ff00"} className={font.className}>
+                    {title}
+                </Text>
+                <Text size="sm" c="dimmed" lh={1.6} className={classes.Descript} ta={"center"}>
+                    {description}
+                </Text>
+                <Title className={classes.titleOverlay} role="presentation">
+                           {Number}
+                        </Title>
+            </Card>
+        </div>
+    );
+}
+
+export function VoiceitWorks() {
     const features = MOCKDATA.map((feature, index) => <Feature {...feature} key={index} />);
 
     return (
-      <Container className={classes.wrapper} fluid p={30}>        
-  
-        <SimpleGrid
-          mt={60}
-          cols={{ base: 1, sm: 2, md: 3 }}
-          spacing={{ base: 'xl', md: 50 }}
-          verticalSpacing={{ base: 'xl', md: 50 }}
-        >
-            <Group>
-                <Card size="lg" shadow="xs" padding="xl" radius="lg" bg={'red'}>
-                    <Title order={1} className={classes.title}>
-                    How It Works:
-                    </Title>
-                    
-                </Card>
-            </Group>
-          {features}
-        </SimpleGrid>
-      </Container>
-  )
+        <Container className={classes.wrapper} fluid >
+
+            <SimpleGrid
+                mt={60}
+                cols={{ base: 1, sm: 2, md: 3 }}
+                spacing={{ base: 'xl', md: 50 }}
+                verticalSpacing={{ base: 'xl', md: 50 }}
+            >
+                <Group>
+                    <Card padding="md" bg={'black'} size="lg">
+                        <Title className={clsx(classes.Title, font.className)}>
+                            How It Works<span className={classes.questionmark}>?</span>
+                        </Title>                        
+                        <Text mt={10} fz={15} className={classes.Descript}>Activate the Voice Assistant with a single click or voice command to navigate your website hands-free, streamlining your browsing experience with intuitive voice control.</Text>
+                        
+                    </Card>
+                </Group>
+                {features}
+            </SimpleGrid>
+        </Container>
+    )
 }
 
 export default VoiceitWorks
