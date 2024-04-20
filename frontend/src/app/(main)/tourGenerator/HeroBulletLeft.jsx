6d81cfb086f308d984cf440967d1c7d0c54e0327
sachin2745 <sachin.official648@gmail.com>
2024-04-20 11:34:13 +0530

    HeroBulletLeft is changed in all

--- frontend/src/app/(main)/tourGenerator/HeroBulletLeft.jsx
+++ frontend/src/app/(main)/tourGenerator/HeroBulletLeft.jsx
@@ -19,10 +19,9 @@ const HeroBulletLeft = () => {
                 </Group>
 
                 <Group className={classes.Left}>
-                    <Text c='#66FF00'  >Use Cases</Text>
                     <Title className={classes.Title}> Enhance Your Projects with Voice Assistant</Title>
                     <p className={classes.P}> Discover seamless website navigation like never before with our Voice Assistant feature. Say goodbye to traditional browsing methods and embrace the future of hands-free interaction. Experience convenience, efficiency, and accessibility at your fingertips.</p>
-                    <Paper shadow="xs" radius="xs" className={classes.paper}>
+                       
                         <Flex
                             mih={50}
                             gap="md"
@@ -32,49 +31,13 @@ const HeroBulletLeft = () => {
                             direction="row"
                             mb={10}
                         >
-                            <Button  variant="outline" color="#66FF00" radius="md">AI Voice Over for Videos</Button>
-                            <Button  variant="outline" color="#66FF00" radius="md">Audio Publishing</Button>
-                            <Button  variant="outline" color="#66FF00" radius="md">Audio story Telling</Button>
+                            <Button className={classes.Button} >Conversational AI</Button>
+                            <Button className={classes.Button} >custom Voice Creation</Button>
+                            <Button className={classes.Button} >E-Learning</Button>
+                            <Button className={classes.Button} >Podcasts</Button>
                         </Flex>
-                        <Flex
-                            mih={50}
-                            gap="md"
-                            wrap={'wrap'}
-                            justify="flex-start"
-                            align="flex-start"
-                            direction="row"
-                            mb={10}
-                        >
-                            <Button  variant="outline" color="#66FF00" radius="md">Conversational AI</Button>
-                            <Button  variant="outline" color="#66FF00" radius="md">custom Voice Creation</Button>
-                            <Button  variant="outline" color="#66FF00" radius="md">E-Learning</Button>
-                            <Button  variant="outline" color="#66FF00" radius="md">Podcasts</Button>
-                        </Flex>
-                        <Flex
-                            mih={50}
-                            gap="md"
-                            wrap={'wrap'}
-                            justify="flex-start"
-                            align="flex-start"
-                            direction="row"
-                            mb={10}>
-                            <Button variant="outline" color="#66FF00" radius="md">Gaming</Button>
-                            <Button variant="outline" color="#66FF00" radius="md">IVR Systems</Button>
-                            <Button variant="outline" color="#66FF00" radius="md">Translation & Dubbing</Button>
-                            <Button variant="outline" color="#66FF00" radius="md">Voice Accesbility</Button>
-                        </Flex>
-                        <Flex
-                            mih={50}
-                            gap="md"
-                            wrap={'wrap'}
-                            justify="flex-start"
-                            align="flex-start"
-                            direction="row" 
-                            mb={10}>
-                            <Button variant="outline" color="#66FF00" radius="md">Voice API</Button>
-
-                        </Flex>
-                    </Paper>
+                       
+                      
                 </Group>
             </div>
         </Container>
--- frontend/src/app/HeroBulletLeft.jsx
+++ frontend/src/app/HeroBulletLeft.jsx
@@ -6,7 +6,7 @@ import classes from './HeroBulletLeft.module.css';
 
 const HeroBulletLeft = () => {
     return (
-        <Container fluid className={classes.Container} pt={80} pb={180} >
+        <Container fluid className={classes.Container}>
             <div className={classes.inner}>
                 <Group className={classes.Right}>
                     <Paper shadow="xs" radius="xs" p="xl" className={classes.Paper}>
@@ -19,13 +19,11 @@ const HeroBulletLeft = () => {
                 </Group>
 
                 <Group className={classes.Left}>
-                    <Text c='#66FF00'  >Use Cases</Text>
-                    <Title className={classes.Title}>Enhance Your Projects with Tour Generator</Title>
-                    <p className={classes.P}> Take control of your website exploration with our Tour Generator feature. Customize your browsing experience by creating personalized tours that guide you through the most relevant sections of any website. Explore with ease and efficiency, tailored to your interests and preferences.
-                    </p>
-                    <Paper shadow="xs" radius="xs" className={classes.paper}>
+                    <Title className={classes.Title}> Enhance Your Projects with Voice Assistant</Title>
+                    <p className={classes.P}> Discover seamless website navigation like never before with our Voice Assistant feature. Say goodbye to traditional browsing methods and embrace the future of hands-free interaction. Experience convenience, efficiency, and accessibility at your fingertips.</p>
+                       
                         <Flex
-                            mih={40}
+                            mih={50}
                             gap="md"
                             wrap={'wrap'}
                             justify="flex-start"
@@ -33,49 +31,13 @@ const HeroBulletLeft = () => {
                             direction="row"
                             mb={10}
                         >
-                            <Button variant="outline" color="#66FF00" radius="md">AI Voice Over for Videos</Button>
-                            <Button variant="outline" color="#66FF00" radius="md">Audio Publishing</Button>
-                            <Button variant="outline" color="#66FF00" radius="md">Audio story Telling</Button>
+                            <Button className={classes.Button} >Conversational AI</Button>
+                            <Button className={classes.Button} >custom Voice Creation</Button>
+                            <Button className={classes.Button} >E-Learning</Button>
+                            <Button className={classes.Button} >Podcasts</Button>
                         </Flex>
-                        <Flex
-                            mih={40}
-                            gap="md"
-                            wrap={'wrap'}
-                            justify="flex-start"
-                            align="flex-start"
-                            direction="row"
-                            mb={10}
-                        >
-                            <Button variant="outline" color="#66FF00" radius="md">Conversational AI</Button>
-                            <Button variant="outline" color="#66FF00" radius="md">custom Voice Creation</Button>
-                            <Button variant="outline" color="#66FF00" radius="md">E-Learning</Button>
-                            <Button variant="outline" color="#66FF00" radius="md">Podcasts</Button>
-                        </Flex>
-                        <Flex
-                            mih={40}
-                            gap="md"
-                            wrap={'wrap'}
-                            justify="flex-start"
-                            align="flex-start"
-                            direction="row"
-                            mb={10}>
-                            <Button variant="outline" color="#66FF00" radius="md">Gaming</Button>
-                            <Button variant="outline" color="#66FF00" radius="md">IVR Systems</Button>
-                            <Button variant="outline" color="#66FF00" radius="md">Translation & Dubbing</Button>
-                            <Button variant="outline" color="#66FF00" radius="md">Voice Accesbility</Button>
-                        </Flex>
-                        <Flex
-                            mih={40}
-                            gap="md"
-                            wrap={'wrap'}
-                            justify="flex-start"
-                            align="flex-start"
-                            direction="row"
-                            mb={10}>
-                            <Button variant="outline" color="#66FF00" radius="md">Voice API</Button>
-
-                        </Flex>
-                    </Paper>
+                       
+                      
                 </Group>
             </div>
         </Container>
