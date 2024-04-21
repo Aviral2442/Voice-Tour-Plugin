09559f0aebedcb17f38064e076566d32e8688b08
Aviral2442 <aviralsrivastava24y15@gmail.com>
2024-04-21 08:47:36 +0530

    tour page maintainance

--- frontend/src/app/(main)/tourGenerator/page.jsx
+++ frontend/src/app/(main)/tourGenerator/page.jsx
@@ -29,7 +29,7 @@ const page = () => {
       <>
         <Navbar />
 
-        <Container>
+        <Container className={classes.mainContainer}>
           <div className={classes.inner}>
             <div className={classes.content}>
               <Title className={classes.title}>
@@ -37,34 +37,10 @@ const page = () => {
                 {/* Welcome to the */}
               </Title>
               <Text c="dimmed" mt="md">
-                Where you'll embark on a journey into the future of website navigation. Our innovative Voice Assistant feature revolutionizes the way you interact with websites by harnessing the power of natural language commands. Say goodbye to cumbersome mouse clicks and keyboard typing – with our Voice Assistant, browsing becomes intuitive and effortless.</Text>
-
-              {/* <List
-            mt={30}
-            spacing="sm"
-            size="sm"
-            icon={
-              <ThemeIcon size={20} radius="xl">
-                <IconCheck
-                  style={{ width: rem(12), height: rem(12) }}
-                  stroke={1.5}
-                />
-              </ThemeIcon>
-            }
-          >
-            <List.Item>
-              <b>TypeScript based</b> – build type safe applications, all
-              components and hooks export types
-            </List.Item>
-            <List.Item>
-              <b>Free and open source</b> – all packages have MIT license, you
-              can use Mantine in any project
-            </List.Item>
-            <List.Item>
-              <b>No annoying focus ring</b> – focus ring will appear only when
-              user navigates with keyboard
-            </List.Item>
-          </List> */}
+                Where you'll embark on a journey into the future of website navigation. Our innovative Voice Assistant feature 
+                revolutionizes the way you interact with websites by harnessing the power of natural language commands. Say goodbye to 
+                cumbersome mouse clicks and keyboard typing – with our Voice Assistant, browsing becomes intuitive and effortless.
+              </Text>
 
               <Group mt={30}>
                 <Button
@@ -101,7 +77,7 @@ const page = () => {
                 </Button>
               </Group>
             </div>
-            {/* <Image src={image.src} className={classes.image} /> */}
+            <Image src='tourSec1.png' className={classes.image}/>
           </div>
         </Container>
 
