f3c2fec95bc6d82b8854fff09f6802a481ab3c6e
sachin2745 <sachin.official648@gmail.com>
2024-04-24 19:28:29 +0530

    some id for search

--- plugin/src/SearchPlugin.jsx
+++ plugin/src/SearchPlugin.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const SearchPlugin = () => {
-    const id = '76526';
+  const id = '660961a3904982acd969fda5';
+
+  const fetchtourData = () => {
+
+    fetch('http://localhost:5000/webpage/getbyuser/' + userid)
+      .then((response) => {
+        console.log(response.status);
+        return response.json();
+      })
+      .then((data) => {
+         console.log(data);
+       
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  useEffect(() => {
+    fetchtourData();
+  }, []);
+
   return (
     <div>SearchPlugin</div>
   )
--- plugin/src/main.jsx
+++ plugin/src/main.jsx
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import TourGenerator from './TourGenerator.jsx';
+import SearchPlugin from './SearchPlugin.jsx';  
 
+// Tour Generator
 const tourEle = document.getElementsByTagName('tour');
 if (tourEle.length > 0) {
   console.log(tourEle[0].attributes);
@@ -16,6 +18,22 @@ if (tourEle.length > 0) {
     <TourGenerator ownerId={ownerId} id={tourId}>
       <div dangerouslySetInnerHTML={{ __html: tourEle[0].innerHTML }} />
     </TourGenerator>
+  )
+}
+
+// Search Plugin
+const searchPlug = document.getElementsByTagName('search');
+if (searchPlug.length > 0) {
+  console.log(searchPlug[0].attributes);
+  const ownerId = searchPlug[0].attributes.getNamedItem('owner-id').value;
+  // const webpageId = searchPlug[0].attributes.getNamedItem('webpage-id').value;
+
+
+  console.log(ownerId);
+  ReactDOM.createRoot(searchPlug[0]).render(
+    <SearchPlugin ownerId={ownerId}>
+      <div dangerouslySetInnerHTML={{ __html: searchPlug[0].innerHTML }} />
+    </SearchPlugin>
   )
 }
 
