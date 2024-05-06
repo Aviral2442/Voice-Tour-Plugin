95a63c4e1270e3d5e925d9f77f48b28ac3bbcece
sachin2745 <sachin.official648@gmail.com>
2024-05-06 12:24:34 +0530

    Search button is working in navbar

--- frontend/src/app/(main)/navbar.jsx
+++ frontend/src/app/(main)/navbar.jsx
@@ -25,7 +25,6 @@ import {
   Menu,
   Avatar,
 } from '@mantine/core';
-import { MantineLogo } from '@mantinex/mantine-logo';
 import { useDisclosure } from '@mantine/hooks';
 import {
   IconNotification,
@@ -50,51 +49,52 @@ import { useState } from "react"
 import { Spotlight, spotlight } from "@mantine/spotlight"
 import '@mantine/spotlight/styles.css';
 import useAppContext from '@/context/AppContext';
-import { IconSettings } from '@tabler/icons-react';
+import { useRouter } from 'next/navigation';
 
 const font = Cormorant_Garamond({ subsets: ['latin'], weight: ['300', '400', '500', '600', '700'] });
 
 
-const mockdata = [
-  {
-    icon: IconCode,
-    title: 'Open source',
-    description: 'This Pokémon’s cry is very loud and distracting',
-  },
-  {
-    icon: IconCoin,
-    title: 'Free for everyone',
-    description: 'The fluid of Smeargle’s tail secretions changes',
-  },
-  {
-    icon: IconBook,
-    title: 'Documentation',
-    description: 'Yanma is capable of seeing 360 degrees without',
-  },
-  {
-    icon: IconFingerprint,
-    title: 'Security',
-    description: 'The shell’s rounded shape and the grooves on its.',
-  },
-  {
-    icon: IconChartPie3,
-    title: 'Analytics',
-    description: 'This Pokémon uses its flying ability to quickly chase',
-  },
-  {
-    icon: IconNotification,
-    title: 'Notifications',
-    description: 'Combusken battles with the intensely hot flames it spews',
-  },
-];
+// const mockdata = [
+//   {
+//     icon: IconCode,
+//     title: 'Open source',
+//     description: 'This Pokémon’s cry is very loud and distracting',
+//   },
+//   {
+//     icon: IconCoin,
+//     title: 'Free for everyone',
+//     description: 'The fluid of Smeargle’s tail secretions changes',
+//   },
+//   {
+//     icon: IconBook,
+//     title: 'Documentation',
+//     description: 'Yanma is capable of seeing 360 degrees without',
+//   },
+//   {
+//     icon: IconFingerprint,
+//     title: 'Security',
+//     description: 'The shell’s rounded shape and the grooves on its.',
+//   },
+//   {
+//     icon: IconChartPie3,
+//     title: 'Analytics',
+//     description: 'This Pokémon uses its flying ability to quickly chase',
+//   },
+//   {
+//     icon: IconNotification,
+//     title: 'Notifications',
+//     description: 'Combusken battles with the intensely hot flames it spews',
+//   },
+// ];
 
 const data = [
-  "Home",
-  "About us",
-  "Contacts",
-  "Blog",
-  "Careers",
-  "Terms of service"
+
+  { label: "Home", link: "/" },
+  { label: "Voice Assistant", link: "/voiceAssistant" },
+  { label: "Tour Generator", link: "/tourGenerator" },
+  { label: "About Us", link: "/about" },
+  { label: "Contact Us", link: "/contact" },
+
 ]
 
 function Navbar() {
@@ -107,34 +107,42 @@ function Navbar() {
   const { setColorScheme } = useMantineColorScheme()
   const computedColorScheme = useComputedColorScheme('light', { getInitialValueInEffect: true });
 
-  const links = mockdata.map((item) => (
-    <UnstyledButton className={classes.subLink} key={item.title}>
-      <Group wrap="nowrap" align="flex-start">
-        <ThemeIcon size={34} variant="default" radius="md">
-          <item.icon style={{ width: rem(22), height: rem(22) }} color={theme.colors.blue[6]} />
-        </ThemeIcon>
-        <div>
-          <Text size="sm" fw={500}>
-            {item.title}
-          </Text>
-          <Text size="xs" c="dimmed">
-            {item.description}
-          </Text>
-        </div>
-      </Group>
-    </UnstyledButton>
+  // const links = mockdata.map((item) => (
+  //   <UnstyledButton className={classes.subLink} key={item.title}>
+  //     <Group wrap="nowrap" align="flex-start">
+  //       <ThemeIcon size={34} variant="default" radius="md">
+  //         <item.icon style={{ width: rem(22), height: rem(22) }} color={theme.colors.blue[6]} />
+  //       </ThemeIcon>
+  //       <div>
+  //         <Text size="sm" fw={500}>
+  //           {item.title}
+  //         </Text>
+  //         <Text size="xs" c="dimmed">
+  //           {item.description}
+  //         </Text>
+  //       </div>
+  //     </Group>
+  //   </UnstyledButton>
 
 
 
-  ));
+  // ));
 
   const [query, setQuery] = useState("");
 
   const [userMenuOpened, setUserMenuOpened] = useState(false);
 
+  const router = useRouter();
+
   const items = data
-    .filter(item => item.toLowerCase().includes(query.toLowerCase().trim()))
-    .map(item => <Spotlight.Action key={item} label={item} />)
+    // .filter(item => item.toLowerCase().includes(query.toLowerCase().trim()))
+    .map(item => <Spotlight.Action
+      {...item}
+      key={item.label}
+      onClick={() => router.push(item.link)}
+    />)
+
+
 
 
   const displayLoginOptions = () => {
@@ -152,7 +160,7 @@ function Navbar() {
             className={cx(classes.user, { [classes.userActive]: userMenuOpened })}
           >
             <Group gap={7}>
-              <Avatar src={'http://localhost:5000/'+currentUser.avatar} alt={currentUser.name} radius="xl" size={40} />
+              <Avatar src={'http://localhost:5000/' + currentUser.avatar} alt={currentUser.name} radius="xl" size={40} />
               <Text fw={500} size="sm" lh={1} mr={3}>
                 {currentUser.name}
               </Text>
@@ -290,40 +298,51 @@ function Navbar() {
         onClose={closeDrawer}
         size="100%"
         padding="md"
-        title="Navigation"
+        title="VoiceTour Navigator"
         hiddenFrom="sm"
         zIndex={1000000}
       >
         <ScrollArea h={`calc(100vh - ${rem(80)})`} mx="-md">
           <Divider my="sm" />
 
-          <a href="#" className={classes.link}>
+          <a href="/" className={classes.link}>
             Home
           </a>
-          <UnstyledButton className={classes.link} onClick={toggleLinks}>
+          <a href="/voiceAssistant" className={classes.link}>
+            Voice Assistant
+          </a>
+          {/* <UnstyledButton className={classes.link} onClick={toggleLinks}>
             <Center inline>
               <Box component="span" mr={5}>
-                Features
+                Voice Assistant
               </Box>
-              <IconChevronDown
-                style={{ width: rem(16), height: rem(16) }}
-                color={theme.colors.blue[6]}
-              />
+             
             </Center>
-          </UnstyledButton>
-          <Collapse in={linksOpened}>{links}</Collapse>
-          <a href="#" className={classes.link}>
-            Learn
+          </UnstyledButton> */}
+          {/* <Collapse in={linksOpened}>{links}</Collapse> */}
+          <a href="/tourGenerator" className={classes.link}>
+            Tour Generator
           </a>
-          <a href="#" className={classes.link}>
-            Academy
+          <a href="/about" className={classes.link}>
+            About Us
+          </a>
+          <a href="/contact" className={classes.link}>
+            Contact Us
           </a>
 
           <Divider my="sm" />
 
           <Group justify="center" grow pb="xl" px="md">
-            <Button variant="default">Log in</Button>
-            <Button>Sign up</Button>
+            <Button variant="default" color='black '>
+              <a href="/login" >
+                Log In
+              </a>
+            </Button>
+            <Button variant='white' color='#66ff00'>
+              <a href="/signup" >
+                Sign Up
+              </a>
+            </Button>
           </Group>
         </ScrollArea>
       </Drawer>
