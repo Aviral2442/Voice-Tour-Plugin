20b678246672bcb4a3b151490add2c1fd3bbefef
Aviral2442 <aviralsrivastava24y15@gmail.com>
2024-05-21 10:58:29 +0530

    tour plugin removed from plugin folder and BOTH PLUGINS are working properly

--- plugin/src/App.jsx
+++ plugin/src/App.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
-// import TourGenerator from './TourGenerator'
 
 const App = () => {
   return (
     <div>
-      {/* <TourGenerator ownerId="123" id="662260eea373067b3190ca92" />       */}Voice Plugin
     </div>
   )
 }
--- plugin/src/main.jsx
+++ plugin/src/main.jsx
@@ -2,24 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-// import TourGenerator from './TourGenerator.jsx';
 import SearchPlugin from './SearchPlugin.jsx';
 import { MantineProvider } from '@mantine/core';
 import '@mantine/core/styles.css';
 import { VoiceProvider } from './VoiceContext.jsx';
-
-// Tour Generator
-// const tourEle = document.getElementsByTagName('tour');
-// if (tourEle.length > 0) {
-//   console.log(tourEle[0].attributes);
-//   const tourId = tourEle[0].attributes.getNamedItem('tour-id').value;
-
-//   ReactDOM.createRoot(tourEle[0]).render(
-//     <TourGenerator id={tourId}>
-//       <div dangerouslySetInnerHTML={{ __html: tourEle[0].innerHTML }} />
-//     </TourGenerator>
-//   )
-// }
 
 // Search Plugin
 const searchPlug = document.getElementsByTagName('search');
@@ -40,9 +26,3 @@ if (searchPlug.length > 0) {
     </MantineProvider>
   )
 }
-
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-// )
