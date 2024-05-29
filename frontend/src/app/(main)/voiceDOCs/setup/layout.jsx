f5a30b63818f661832b40818b630262b86e74b98
Aviral2442 <aviralsrivastava24y15@gmail.com>
2024-05-29 13:35:19 +0530

    setup page

---
+++ frontend/src/app/(main)/voiceDOCs/setup/layout.jsx
@@ -0,0 +1,30 @@
+// 'use client'
+// import React from 'react'
+// import { AppShell } from "@mantine/core"
+// import SetupTOC from './setupTOC'
+
+// const Layout = ({children}) => {
+//   return (
+//     <AppShell
+//     header={{width:0}}
+//       aside={{ 
+//         width: 250, 
+//         breakpoint: "lg" 
+//       }}
+//     >
+//       <AppShell.Main >{children}</AppShell.Main>
+//       <AppShell.Aside  withBorder={false} ><SetupTOC/></AppShell.Aside>
+//     </AppShell>
+//   )
+// }
+
+// export default Layout
+import React from 'react'
+
+const Layout = ({children}) => {
+  return (
+    <div>{children}</div>
+  )
+}
+
+export default Layout
---
+++ frontend/src/app/(main)/voiceDOCs/setup/setupTOC.jsx
@@ -0,0 +1,10 @@
+'use client'
+import React from 'react'
+
+const SetupTOC = () => {
+  return (
+    <div>SetupTOC</div>
+  )
+}
+
+export default SetupTOC
